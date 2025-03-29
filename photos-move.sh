33de8d94c15e90e06dcc0c85f38e6635ab8da2a6
Andrew D'Amario <51063733+andrewaclear@users.noreply.github.com>
2025-03-28 22:10:44 -0400

    upgrade to detect duplicates prefixed with the same date (can have different original names)

--- photos-move.sh
+++ photos-move.sh
@@ -13,6 +13,8 @@ fi
 count=0
 
 while IFS= read -r file; do
+  [[ -f $file ]] || continue
+  unset found_file
   base=$(basename "$file")
   date=$(exiftool -s -s -s -d "%Y-%m-%d-%H%M%S" -DateTimeOriginal -CreateDate -FileModifyDate "$file" | grep -Ev "0000-|0000:" | head -n 1)
   [[ $date == "" ]] &&
@@ -24,18 +26,29 @@ while IFS= read -r file; do
   [[ -d "${DESTINATION_DIR}/${year}/${month}" ]] || mkdir "${DESTINATION_DIR}/${year}/${month}"
 
   new_file="${DESTINATION_DIR}/${year}/${month}/${date}_${base}"
-  if [[ -f "$new_file" ]]; then
-    echo -n "removing file://${file} if it is the same, file://${new_file} exists "
-    diff <(exiftool "$file" | grep -Ev "File|Directory") <(exiftool "$new_file" | grep -Ev "File|Directory") ||
-      { echo -e "\nERROR: file exifs are not the same: file://${file} and file://${new_file}"; exit 1; }
-    cmp -s "$file" "$new_file" ||
-      { echo -e "\nERROR: files are not the same: file://${file} and file://${new_file}"; exit 1; }
-    rm "$file" ||
-      { echo -e "\nERROR: saved file://${new_file}, but could not delete the original file file://${file}"; exit 1; }
-  else
-    mv -n "$file" "$new_file" ||
-      { echo "ERROR: failed to move file://${file} as file://${new_file}"; exit 1; }
-    echo -n "file://${file} -> file://${new_file} "
+  if [[ $(find "${DESTINATION_DIR}/${year}/${month}" -type f -name "${date}*") ]]; then
+    while IFS= read -r duplicate_file; do
+      [[ -f $duplicate_file ]] || continue
+      diff <(exiftool "$file" | grep -Ev "File|Directory") <(exiftool "$duplicate_file" | grep -Ev "File|Directory") > /dev/null || continue 
+      cmp -s "$file" "$duplicate_file" > /dev/null || continue
+      rm "$file" ||
+        { echo -e "\nERROR: saved file://${duplicate_file}, but could not delete the original file file://${file}"; exit 1; }
+      echo -n "removed file://${file}, it is the same as file://${duplicate_file} "
+      found_file=true
+      break
+    done <<< $(echo $new_file; find "${DESTINATION_DIR}/${year}/${month}" -type f -name "${date}*")
+  fi
+  if [[ ! $found_file ]]; then
+    if [[ ! -f $new_file ]]; then
+      mv -n "$file" "$new_file" ||
+        { echo "ERROR: failed to move file://${file} as file://${new_file}"; exit 1; }
+      echo -n "file://${file} -> file://${new_file} "
+    else
+      diff <(exiftool "$file" | grep -Ev "File|Directory") <(exiftool "$new_file" | grep -Ev "File|Directory")
+      cmp -s "$file" "$new_file"
+      echo -e "\nERROR: files are not the same: file://${file} and file://${new_file}"
+      exit 1;
+    fi
   fi
 
   count=$(($count + 1))
